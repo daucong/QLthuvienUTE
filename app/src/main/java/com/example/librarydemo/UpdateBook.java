c7c6a4539346c22b95765629ff4f91a2a55b775c
Công Đậu <dauthaicong11092001@gmail.com>
2021-11-17 09:57:50 +0700

    click image -> camera

--- app/src/main/java/com/example/librarydemo/UpdateBook.java
+++ app/src/main/java/com/example/librarydemo/UpdateBook.java
@@ -1,15 +1,55 @@
 package com.example.librarydemo;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateBook extends AppCompatActivity {
-
+    Button btnThem, btnThoat;
+    ImageView imvHinh;
+    EditText edTensach, edTheLoai, edNamXB, edTacGia, edSoluong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_book);
+        Anhxa();
+
+
+        imvHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 8888);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==8888 && requestCode==RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imvHinh.setImageBitmap(photo);
+        }
+    }
+
+    public void Anhxa(){
+        btnThem = (Button) findViewById(R.id.tt_btThem);
+        btnThoat = (Button) findViewById(R.id.tt_bthuy);
+        imvHinh = (ImageView) findViewById(R.id.tt_img_Anh);
+        edTensach = (EditText) findViewById(R.id.edit_tensach);
+        edTheLoai = (EditText) findViewById(R.id.tt_TheLoai);
+        edNamXB = (EditText) findViewById(R.id.edit_nxb);
+        edTacGia = (EditText) findViewById(R.id.edit_tacgia);
+        edSoluong = (EditText) findViewById(R.id.edit_soluong);
+
     }
 }
